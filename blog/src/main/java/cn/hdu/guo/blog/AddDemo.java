9bc989cdd47a829db0d8f19855e9570064429814
guoyebin <guoyebin@hikvision.com.cn>
2021-07-03 17:23:00 +0800

    [add]增加文件提交。

---
+++ blog/src/main/java/cn/hdu/guo/blog/AddDemo.java
@@ -0,0 +1,16 @@
+package cn.hdu.guo.blog;
+
+/**
+ * 功能描述
+ *
+ * @author guoyebin
+ * @company 海康威视
+ * @date 17:21 2021/7/3
+ */
+public class AddDemo {
+	public static void main(String[] args) {
+		System.out.println("yes");
+	}
+
+
+}
